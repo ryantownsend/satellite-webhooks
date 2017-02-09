80379abb607ef7990b12bafdd633f253af442861
Ryan Townsend <ryan@ryantownsend.co.uk>
2017-02-09 21:40:54 +0000

    Feature: HTTP Basic Auth support for webhook delivery (#2)

--- app/resources/api/v1/webhook_resource.rb
+++ app/resources/api/v1/webhook_resource.rb
@@ -1,6 +1,18 @@
 class Api::V1::WebhookResource < JSONAPI::Resource
-  attributes :identifier, :url, :headers, :body
+  attributes :identifier, :url, :headers, :body, :auth
 
   # associations
   has_many :deliveries
+
+  def auth
+    {
+      username: _model.basic_auth_username,
+      password: _model.basic_auth_password
+    }.compact
+  end
+
+  def auth=(value)
+    _model.basic_auth_username = value[:username]
+    _model.basic_auth_password = value[:password]
+  end
 end
